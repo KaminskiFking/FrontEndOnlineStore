3b217a2264681d3e1b4b3960fb35d560144c07fd
Marcos Furtado <78755108+MarcosFFurtado@users.noreply.github.com>
2022-08-05 15:47:00 -0300

    req 1

--- src/services/api.js
+++ src/services/api.js
@@ -1,17 +1,15 @@
 export async function getCategories() {
-    const categories = await fetch(`https://api.mercadolibre.com/sites/MLB/categories`)
-      .then((awnser) => awnser.json())
-      .then((data) => data)
-      .catch((error) => error);
-      return categories;
-    };
+  const categories = await fetch('https://api.mercadolibre.com/sites/MLB/categories')
+    .then((awnser) => awnser.json())
+    .then((data) => data)
+    .catch((error) => error);
+  return categories;
+}
 
-    
-
-export async function getProductsFromCategoryAndQuery(categoryId, query) {
+export async function getProductsFromCategoryAndQuery(categoryId) {
   const cat = await fetch(` https://api.mercadolibre.com/sites/MLB/search?category=$CATEGORY_ID&q=${categoryId}`)
-      .then((awnser) => awnser.json())
-      .then((data) => data)
-      .catch((error) => error);
-      return cat;
-    };
+    .then((awnser) => awnser.json())
+    .then((data) => data)
+    .catch((error) => error);
+  return cat;
+}
