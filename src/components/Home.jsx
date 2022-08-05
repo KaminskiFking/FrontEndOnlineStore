e0ddf03a2e9e90786410afea7918b79d14e0b5ae
Marcos Furtado <78755108+MarcosFFurtado@users.noreply.github.com>
2022-08-05 19:28:20 -0300

    req 5

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-import { getCategories } from '../services/api';
+import { getCategories, getProductsFromCategoryAndQuery } from '../services/api';
 
 export default class Home extends Component {
   constructor() {
     super();
     this.state = {
       categorie: [],
+      nameText: '',
+      products: [],
     };
   }
 
@@ -17,27 +19,48 @@ export default class Home extends Component {
     });
   }
 
+  onChange = ({ target }) => {
+    this.setState({
+      nameText: target.value,
+    });
+  }
+
+  fetchProducts = async () => {
+    const { nameText } = this.state;
+    const prod = await getProductsFromCategoryAndQuery(nameText);
+    const { results } = prod;
+    this.setState({
+      products: results,
+    });
+  }
+
   render() {
-    const { categorie } = this.state;
+    const { categorie, nameText, products } = this.state;
+    console.log(products);
     return (
       <div>
         <div data-testid="home-initial-message">
           Digite algum termo de pesquisa ou escolha uma categoria.
         </div>
         <div>
-          {console.log(categorie)}
-        </div>
-        <div>
           <label htmlFor="text">
             Produto
             <input
-              name="iproduct"
+              data-testid="query-input"
+              name="product"
               id="product"
               type="text"
-            // value={ inputName }
-            // onChange={ this.onInputChange }
+              value={ nameText }
+              onChange={ this.onChange }
             />
           </label>
+          <button
+            data-testid="query-button"
+            type="button"
+            onClick={ this.fetchProducts }
+          >
+            Buscar
+          </button>
           <Link
             data-testid="shopping-cart-button"
             to="/search"
@@ -51,10 +74,18 @@ export default class Home extends Component {
               data-testid="category"
               type="button"
             >
-              { cat.name }
+              {cat.name}
             </button>
           ))}
         </div>
+        {products.length !== 0 ? (
+          products.map((item, index) => (
+            <div data-testid="product" key={ index }>
+              <p>{item.title}</p>
+              <img src={ item.thumbnail } alt={ item.title } />
+              <p>{item.price}</p>
+            </div>
+          ))) : <p>Nenhum produto foi encontrado</p>}
       </div>
     );
   }
